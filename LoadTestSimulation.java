078980263dbb2d500fee1b9b213be68494f75723
AmitEliya <167574726+AmitEliya@users.noreply.github.com>
2025-05-20 13:20:21 +0300

    Update LoadTestSimulation.java

--- LoadTestSimulation.java
+++ LoadTestSimulation.java
@@ -1,31 +1,29 @@
 import java.time.Duration;
-import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.http.HttpDsl.*;
-
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
 public class LoadTestSimulation extends Simulation {
 
-  private HttpProtocolBuilder httpProtocol = http
-    .baseUrl("http://34.58.208.53:8080")
-    .inferHtmlResources()
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8")
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.9,he;q=0.8")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+    HttpProtocolBuilder httpProtocol = http
+        .baseUrl("http://34.58.208.53:8080")
+        .inferHtmlResources();
 
-  private ScenarioBuilder scn = scenario("LoadTestSimulation")
-    .exec(
-      http("Request Home Page")
-        .get("/index.jsp")
-    );
+    ScenarioBuilder scn = scenario("LoadTestSimulation")
+        .exec(
+            http("Request Home Page")
+            .get("/index.jsp")
+        );
 
-  {
-    setUp(
-      scn.injectOpen(
-        constantUsersPerSec(700).during(Duration.ofMinutes(5))
-      ).protocols(httpProtocol)
-    );
-  }
+    {
+        setUp(
+            scn.injectOpen(
+                rampUsersPerSec(1).to(750).during(Duration.ofMinutes(2)), 
+                constantUsersPerSec(750).during(Duration.ofMinutes(2)),    
+                rampUsersPerSec(750).to(1).during(Duration.ofMinutes(1))  
+            )
+        ).protocols(httpProtocol);
+    }
 }
